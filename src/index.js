dc982c962c66f4359b1285a8857a98f23edd9c71
Luis Zepeda <luis@picnicdigital.io>
2021-07-21 23:03:32 -0500

    chore(): added StyleLint, ESLint, Prettier and precommit hook for linter validations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render("Hi react!", document.getElementById("app"));
+export const App = () => {
+  return (
+    <div>
+      <h1>Que tal mi gente de internet!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
