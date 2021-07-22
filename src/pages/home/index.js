adf28021f1890be899c9b7a4e4eac5f64035a45b
Luis Zepeda <luis@picnicdigital.io>
2021-07-22 03:48:38 -0500

    feat: added styled component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHome = styled.div`
+  margin: 0;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <StyledHome>
       <h1>Que onda mi gente d internet!</h1>
-    </div>
+    </StyledHome>
   );
 };
 
